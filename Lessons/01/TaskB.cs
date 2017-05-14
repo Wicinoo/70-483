fc1927fb4afafeee208cf2ffec31c43351ecf1d3
Jakub Holík <xholik1@gmail.com>
2017-05-14 22:21:26 +0200

    taskB

--- Lessons/01/TaskB.cs
+++ Lessons/01/TaskB.cs
@@ -8,9 +8,21 @@ namespace Lessons._01
     /// </summary>
     public class TaskB
     {
+        delegate string MapString(string @string);
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            MapString string1 = delegate (string @string) { return @string; };
+            MapString string2 = (string @string) => { return @string; };
+            MapString string3 = (@string) => { return @string; };
+            MapString string4 = @string => { return @string; };
+            MapString string5 = @string => @string;
+
+            Console.WriteLine(string1("string1"));
+            Console.WriteLine(string2("string2"));
+            Console.WriteLine(string3("string3"));
+            Console.WriteLine(string4("string4"));
+            Console.WriteLine(string5("string5"));
         }
     }
 }
