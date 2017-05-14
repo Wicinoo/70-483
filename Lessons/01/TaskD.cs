7711776cb7c291d33db6fd6e701895252c650386
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2017-05-14 20:52:32 +0200

    Solution TaskD

--- Lessons/01/TaskD.cs
+++ Lessons/01/TaskD.cs
@@ -7,9 +7,36 @@ namespace Lessons._01
     /// </summary>
     public class TaskD
     {
+        delegate Tree GetTree();
+        delegate Oak GetOak();
+        
         public static void Run()
         {
-            throw new NotImplementedException();
+            GetTree getTree = GrowTree;
+            GetOak getOak = CutDownOak;
+
+            getTree();
+            getOak();
+
+            GetTree cutDownOak = CutDownOak;
+            cutDownOak();
+
+            //GetOak growOak = GrowTree;
+        }
+
+        private static Tree GrowTree()
+        {
+            Console.WriteLine("You've grown a tree.");
+            return new Tree();
+        }
+
+        private static Oak CutDownOak()
+        {
+            Console.WriteLine("You've cut down a 300 years old oak :(");
+            return new Oak();
         }
     }
+
+    public class Tree { }
+    public class Oak : Tree { }
 }
