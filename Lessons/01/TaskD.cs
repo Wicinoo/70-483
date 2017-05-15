536e7f4961ee6f461dbb74472c2de40ad9b72345
Jiří Drdla <drdla.jiri@outlook.com>
2017-05-15 23:52:55 +0200

    Task D.

--- Lessons/01/TaskD.cs
+++ Lessons/01/TaskD.cs
@@ -7,9 +7,54 @@ namespace Lessons._01
     /// </summary>
     public class TaskD
     {
+        delegate IDevice GetDevice();
+
+        delegate MobileDevice GetMobileDevice();
+
+        delegate DesktopDevice GetDesktopDevice();
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            GetDevice getDevice = GetRandomDevice;
+            getDevice += GetSomeMobileDevice;
+            getDevice += GetSomeDesktopDevice;
+            getDevice();
+
+            GetMobileDevice getMobileDevice = GetSomeMobileDevice;
+            getMobileDevice();
+
+            GetDesktopDevice getDesktopDevice = GetSomeDesktopDevice;
+            getDesktopDevice();
         }
+
+        static IDevice GetRandomDevice() {
+            if (DateTime.Now.Second % 2 == 1)
+            {
+                return GetSomeMobileDevice();
+            }
+            else
+            {
+                return GetSomeDesktopDevice();
+            }
+        }
+
+        static MobileDevice GetSomeMobileDevice()
+        {
+            Console.WriteLine("Mobile device created.");
+            return new MobileDevice();
+        }
+
+        static DesktopDevice GetSomeDesktopDevice()
+        {
+            Console.WriteLine("Desktop device created.");
+            return new DesktopDevice();
+        }
+
     }
+
+    interface IDevice {}
+
+    class MobileDevice : IDevice {}
+
+    class DesktopDevice : IDevice {}
 }
