7e2a0cf6176d20ab5f528a92e55035c27484a73d
sfield <sfield@fnz.com>
2017-05-15 21:24:41 +0100

    task c complete

--- Lessons/01/Runner.cs
+++ Lessons/01/Runner.cs
@@ -9,15 +9,15 @@ namespace Lessons._01
             //_01_Delegate.Run();
             //_02_OperationsWithDelegates.Run();
             //_03_LambdaExpressionBasics.Run();
-            //_04_Contravariance.Run();s
+            //_04_Contravariance.Run();
             //_05_Covariance.Run();
             //_06_DelegatesAdvancedTopics.Run();
             //_07_SystemDelegates.Run();
             //_08_Events.Run();
 
             //TaskA.Run();
-            TaskB.Run();
-            //TaskC.Run();
+            //TaskB.Run();
+            TaskC.Run();
             //TaskD.Run();
             //TaskE.Run();
             //TaskF.Run();
--- Lessons/01/TaskC.cs
+++ Lessons/01/TaskC.cs
@@ -11,9 +11,42 @@ namespace Lessons._01
     /// </summary>
     public class TaskC
     {
+        delegate void ServiceCar(Car car);
+        delegate void ServiceBus(Bus bus);
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            // delegate requires Car and method requires Car
+            ServiceCar changeTyres = ChangeTyres;
+            changeTyres(new Car());
+            changeTyres(new Bus());
+
+            // delegate requires Car + method requires Bus
+            // It is not possible to pass any car if we expect only buses.
+            //ServiceCar cleanCarInterior = CleanBus;   
+
+            // delegate requires Bus and method requires Car
+            // It is possible because of Contravariance - A bus can have tyres changed
+            ServiceBus changeBusTyres = ChangeTyres;
+            changeBusTyres(new Bus());
+
+            // delegate requires Bus and method requires Bus
+            ServiceBus cleanBusInterior = CleanBus;
+            cleanBusInterior(new Bus());
         }
+
+        private static void ChangeTyres(Car car)
+        {
+            Console.WriteLine($"{car.GetType().Name} tyres changed.");
+        }
+
+        private static void CleanBus(Bus bus)
+        {
+            Console.WriteLine($"{bus.GetType().Name} interior cleaned.");
+        }
+
+        class Car { }
+
+        class Bus : Car { }
     }
 }
