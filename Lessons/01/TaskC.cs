fa7b0d0a0eb3ec0de93d880ae0742897016ddaad
vstusak <vstusak@fnz.com>
2017-05-16 13:57:47 +0200

    Task C

--- Lessons/01/TaskC.cs
+++ Lessons/01/TaskC.cs
@@ -11,9 +11,50 @@ namespace Lessons._01
     /// </summary>
     public class TaskC
     {
+        private delegate void ServiceCar(Car car);
+        private delegate void ServiceBus(Bus bus);
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            // Contravariance - permits use a method with less derived types in argument for a delegate.
+
+            // delegate requires car and method requires car
+            ServiceCar changeTyres = ChangeTyres;
+            changeTyres(new Car());
+            changeTyres(new Bus());
+
+            // It is not possible to pass any car if we expect only bus.
+            //ServiceCar cleanBus = CleanUpBus;      
+
+            // delegate requires Bus and method requires Car
+            // It is possible because of Contravariance - we can change tyres for bus
+            ServiceBus changeTyresBus = ChangeTyres;
+            changeTyresBus(new Bus());
+
+            // delegate requires Bus and method requires Bus
+            ServiceBus cleanUpBus = CleanUpBus;
+            cleanUpBus(new Bus());
         }
+
+        private static void ChangeTyres(Car car)
+        {
+            Console.WriteLine($"Tyres are changed for {car.GetType().Name}");
+        }
+
+        private static void CleanUpBus(Bus bus)
+        {
+            Console.WriteLine($"Interior of {bus.GetType().Name} is cleaned up");
+        }
+    }
+
+
+    class Car
+    {
+         
+    }
+
+    class Bus : Car
+    {
+         
     }
 }
