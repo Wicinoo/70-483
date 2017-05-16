f8bd02167352852eb69c3d4229ed2557ec29f823
tbilzou <tbilzou@users.noreply.github.com>
2017-05-17 00:28:54 +0200

    Update TaskB.cs

--- Lessons/01/TaskB.cs
+++ Lessons/01/TaskB.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Lessons._01
 {
@@ -10,7 +10,27 @@ namespace Lessons._01
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            MapString op1 = s => "op1: " + s;
+            MapString op2 = s => { return "op2: " + s; };
+            MapString op3 = (s) => "op3: " + s;
+            MapString op4 = (string s) => "op4: " + s;
+            MapString op5 = (s) => { return "op5: " + s; };
+            MapString op6 = (string s) => { return "op6: " + s; };
+            MapString op7 = delegate(string s) { return "op7: " + s; };
+
+
+            Console.WriteLine("TaskB:");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(op1("Completed!"));
+            Console.WriteLine(op2("Completed!"));
+            Console.WriteLine(op3("Completed!"));
+            Console.WriteLine(op4("Completed!"));
+            Console.WriteLine(op5("Completed!"));
+            Console.WriteLine(op6("Completed!"));
+            Console.WriteLine(op7("Completed!"));
+            Console.WriteLine("---------------------------------------");
         }
     }
+
+    delegate string MapString(string @string);
 }
