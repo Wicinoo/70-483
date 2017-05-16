e04b06d021bd6a1af0baa7f0cd84d884a952f9ff
rdressle <robert.dresler@fnz.com>
2017-05-16 20:22:24 +0200

    Lesson 02

---
+++ Lessons/02/TaskD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lessons._02
+{
+    /// <summary>
+    /// Set capacity of ThreadPool to 10 and start 15 tasks. Let them run at least 1000 miliseconds. 
+    /// Print out the time used for creation of each task.
+    /// </summary>
+    public class TaskD
+    {
+        public static void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lessons/02/_06_ThreadPoolUsage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Lessons._02
+{
+    public static class _06_ThreadPoolUsage
+    {
+        public static void Run()
+        {
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+
+            int workerThreads;
+            int completionPortThreads;
+
+            ThreadPool.GetMaxThreads(out workerThreads, out completionPortThreads);
+
+            Console.WriteLine("Worked threads: {0}, CompletionPortThreads: {1}", workerThreads, completionPortThreads);
+        }
+    }
+}
