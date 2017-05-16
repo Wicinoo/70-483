f6f42f2a84f069b0f090da6bcf44eeb046d3d843
rdressle <robert.dresler@fnz.com>
2017-05-16 20:22:24 +0200

    Lesson 02

---
+++ Lessons/02/_02_BackgroundThread.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+
+namespace Lessons._02
+{
+    public static class _02_BackgroundThread 
+    {
+        public static void Run()
+        {
+            Thread thread = new Thread(new ThreadStart(DoOnOtherThread));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private static void DoOnOtherThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Other thread: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
