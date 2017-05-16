369a6961b99337daf44f0d45bc153f3ffcb1720f
vstusak <vstusak@fnz.com>
2017-05-16 13:40:54 +0200

    Task B

--- Lessons/01/TaskB.cs
+++ Lessons/01/TaskB.cs
@@ -8,9 +8,22 @@ namespace Lessons._01
     /// </summary>
     public class TaskB
     {
+        delegate string MapString(string @string);
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            // All right-side forms are equivalent.
+            MapString operation1 = delegate (string x) { return x; };   // Anonymous method
+            MapString operation2 = (string x) => { return x; };   // Lambda expression (explicitly typed parameter)
+            MapString operation3 = (x) => { return x; };   // Lambda expression (implicitly typed parameter)
+            MapString operation4 = x => { return x; };   // Lambda expression (implicitly typed parameter)
+            MapString operation5 = x => x;   // Lambda expression (implicitly typed parameter)
+
+            Console.WriteLine($"operation1(MapString) = {operation1("MapString")}");
+            Console.WriteLine($"operation2(MapString) = {operation2("MapString")}");
+            Console.WriteLine($"operation3(MapString) = {operation3("MapString")}");
+            Console.WriteLine($"operation4(MapString) = {operation4("MapString")}");
+            Console.WriteLine($"operation5(MapString) = {operation5("MapString")}");
         }
     }
 }
