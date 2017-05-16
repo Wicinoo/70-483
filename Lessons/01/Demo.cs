457bf6d0d0533b625c481f85ef2a7129994831ec
rdressle <robert.dresler@fnz.com>
2017-05-16 19:24:33 +0200

    Demo for lesson 01

---
+++ Lessons/01/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lessons._01
+{
+    public class Demo
+    {
+        public static void Run()
+        {
+            // # Create a delegate
+            // # Delegate arithmetic
+            // # From lambda to delegate
+            // # Predefined delegates
+            // # Immutability of delegates
+            // # Contravariance, covariance
+            // # Events (vs. properties like "events")
+            // # Add / remove members
+        }
+    }
+}
--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -7,7 +7,10 @@ namespace Lessons
         static void Main(string[] args)
         {
             //_01.Runner.Run(); //uncomment this row for test
+            _01.Demo.Run();
 
+            Console.WriteLine();
+            Console.WriteLine("Press a key ...");
             Console.ReadKey(); 
         }
     }
