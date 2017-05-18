409426fc1c02433292efa671258f177cc4a35f39
jkrampol <jiri.krampol@gmail.com>
2017-05-18 17:16:37 +0100

    Task B&C

--- Lessons/01/TaskB.cs
+++ Lessons/01/TaskB.cs
@@ -8,9 +8,21 @@ namespace Lessons._01
     /// </summary>
     public class TaskB
     {
+        public delegate string MapString(string @string);
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            MapString str1 = delegate(string x) { return x; };
+            MapString str2 = (string x) => { return x; };
+            MapString str3 = (x) => { return x; };
+            MapString str4 = x => { return x; };
+            MapString str5 = x => x;
+
+            Console.WriteLine(str1("str1 example"));
+            Console.WriteLine(str2("str2 example"));
+            Console.WriteLine(str3("str3 example"));
+            Console.WriteLine(str4("str4 example"));
+            Console.WriteLine(str5("str5 example"));
         }
     }
 }
--- Lessons/01/TaskC.cs
+++ Lessons/01/TaskC.cs
@@ -11,9 +11,45 @@ namespace Lessons._01
     /// </summary>
     public class TaskC
     {
+        public delegate void ServiceCar(Car car);
+
+        public delegate void ServiceBus(Bus bus);
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            ServiceCar changeTyres = ChangeTyresOfCar;
+            changeTyres(new Car());
+            changeTyres(new Bus());
+
+            Console.WriteLine();
+
+            //contravariance
+            ServiceBus changeTyresAgain = ChangeTyresOfCar;
+            changeTyresAgain(new Bus());
+            //changeTyresAgain(new Car()); //not possible
+            
+            Console.WriteLine();
+
+            //not possible
+            //ServiceCar cleanInterior = CleanInterierOfBus;
+
+            ServiceBus cleanInteriorAgain = CleanInterierOfBus;
+            //cleanInteriorAgain(new Car()); //not possible
+            cleanInteriorAgain(new Bus());
         }
+
+        private static void ChangeTyresOfCar(Car car)
+        {
+            Console.WriteLine($"Tires of *{car.GetType().Name}* has been changed");
+        }
+
+        private static void CleanInterierOfBus(Bus bus)
+        {
+            Console.WriteLine($"Interior of *{bus.GetType().Name}* has been cleaned up");
+        }
+
+        public class Car { };
+
+        public class Bus : Car { };
     }
 }
