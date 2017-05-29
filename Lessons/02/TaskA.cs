38d791553588bf036124e47b67f16490f79de970
Jiří Drdla <drdla.jiri@outlook.com>
2017-05-29 22:11:52 +0200

    TaskA

--- Lessons/02/TaskA.cs
+++ Lessons/02/TaskA.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Lessons._02
 {
@@ -9,7 +11,18 @@ namespace Lessons._02
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            Task task = Task.Run(() =>
+            {
+                Console.WriteLine($"ApartmentState: {Thread.CurrentThread.GetApartmentState()}");
+                Console.WriteLine($"CurrentCulture: {Thread.CurrentThread.CurrentCulture}");
+                Console.WriteLine($"CurrentUICulture: {Thread.CurrentThread.CurrentUICulture}");
+                Console.WriteLine($"IsAlive: {Thread.CurrentThread.IsAlive}");
+                Console.WriteLine($"IsBackground: {Thread.CurrentThread.IsBackground}");
+                Console.WriteLine($"IsThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+                Console.WriteLine($"ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"Priority: {Thread.CurrentThread.Priority}");
+                Console.WriteLine($"ThreadState: {Thread.CurrentThread.ThreadState}");
+            });
         }
     }
 }
