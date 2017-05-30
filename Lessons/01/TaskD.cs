73cdfcd55f7322c898f46088b35e7dcb78e72600
jkrampol <jiri.krampol@gmail.com>
2017-05-30 14:52:15 +0100

    taskD

--- Lessons/01/TaskD.cs
+++ Lessons/01/TaskD.cs
@@ -7,9 +7,39 @@ namespace Lessons._01
     /// </summary>
     public class TaskD
     {
+        public delegate Vehicle DriveVehicle();
+        public delegate Bus DriveBus();
+
         public static void Run()
         {
-            throw new NotImplementedException();
+            DriveVehicle vehicle = GetVehicleLicence;
+            DriveBus bus = GetBusLicence;
+            DriveVehicle caravan = GetBusLicence;
+
+            var drivingLicence = new[]
+            {
+                vehicle(),
+                bus(),
+                caravan()
+            };
+        }
+
+        private static Vehicle GetVehicleLicence()
+        {
+            Console.WriteLine("Able to drive a Vehicle.");
+            return new Vehicle();
+        }
+
+        private static Bus GetBusLicence()
+        {
+            Console.WriteLine("Able to drive a Bus.");
+            return new Bus();
         }
     }
+
+    public class Vehicle { }
+
+    public class Car : Vehicle { }
+
+    public class Bus : Vehicle { }
 }
