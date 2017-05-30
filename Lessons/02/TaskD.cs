a6ac3c8fb871e0fa53c416917a6a9861ec474e27
vadimyarovikov <vadim.yarovikov@gmail.com>
2017-05-30 09:15:15 +0200

    Lesson02-TaskD

--- Lessons/02/TaskD.cs
+++ Lessons/02/TaskD.cs
@@ -2,6 +2,10 @@
 
 namespace Lessons._02
 {
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// Set capacity of ThreadPool to 10 and start 15 tasks. Let them run at least 1000 miliseconds. 
     /// Print out the time used for creation of each task.
@@ -10,7 +14,21 @@ namespace Lessons._02
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            ThreadPool.SetMaxThreads(10, 10);
+            ThreadPool.SetMinThreads(10, 10);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 1; i <= 15; i++)
+            {
+                ThreadPool.QueueUserWorkItem(
+                    state => Task.Run(
+                        () =>
+                            {
+                                Console.WriteLine($"{sw.Elapsed}");
+                                Thread.Sleep(1000);
+                            }));
+            }
         }
     }
 }
