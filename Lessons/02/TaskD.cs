b9c65a8dd8872fe29ecf63983128f36dea20d3e2
jdrdla <jdrdla@RBE04-B091>
2017-05-30 21:21:59 +0200

    Task C-E.

--- Lessons/02/TaskD.cs
+++ Lessons/02/TaskD.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Lessons._02
 {
@@ -10,7 +13,21 @@ namespace Lessons._02
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            ThreadPool.SetMinThreads(10, 10);
+            ThreadPool.SetMaxThreads(10, 10);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i < 15; i++)
+            {
+                ThreadPool.QueueUserWorkItem(
+                    num => Task.Run(() => {
+                        Console.WriteLine($"Task {num}: {stopwatch.ElapsedMilliseconds} ms");
+                        Thread.Sleep(1000);
+                    }), i
+                );
+            }
         }
     }
 }
