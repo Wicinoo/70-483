66e76588b28c1cf14db0bdae76a19a112f9f40be
Tom067 <Tom067@DESKTOP-OMCN4A0>
2017-05-30 22:16:27 +0200

    Completed Task C

--- Lessons/02/TaskC.cs
+++ Lessons/02/TaskC.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Lessons._02
 {
@@ -11,7 +15,28 @@ namespace Lessons._02
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            var numbers = Enumerable.Range(1, 1000).ToList();
+            try
+            {
+                Parallel.ForEach(numbers, number => Console.WriteLine(number));
+                Parallel.ForEach(numbers, number => numbers.Remove(number));
+            }
+            catch (AggregateException exception)
+            {
+                Console.WriteLine(exception.Message);
+                foreach (var e in exception.InnerExceptions)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Exceptions thrown: " + exception.InnerExceptions.Count);
+            }
+
+            int concurrentNumber = 0;
+            var concurrentNumbers = new ConcurrentBag<int>(Enumerable.Range(1, 1000));
+
+            Parallel.ForEach(concurrentNumbers, number => Console.WriteLine(number));
+            Parallel.ForEach(concurrentNumbers, number => concurrentNumbers.TryTake(out concurrentNumber));
         }
     }
 }
