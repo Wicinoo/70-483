23ac7d9974ea89a40ea366a46697f3d4459e1dc6
tbilzou <tbilzou@users.noreply.github.com>
2017-06-06 08:09:12 +0100

    Create TaskB.cs

---
+++ Lessons/03/TaskB.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace Lessons._03
+{
+    /// <summary>
+    /// Implement IsPowerOfTwo() method to pass all tests. 
+    /// You have to use binary "&" operator in your implementation.
+    /// </summary>
+    public class TaskB
+    {
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(1, true)]
+        [InlineData(2, true)]
+        [InlineData(3, false)]
+        [InlineData(4, true)]
+        [InlineData(5, false)]
+        [InlineData(8, true)]
+        public void IsPowerOfTwo_ForGivenNumber_ShouldReturnExpected(int number, bool expectedIsPowerOfTwo)
+        {
+            var isPowerOfTwo = IsPowerOfTwo(number);
+            Assert.Equal(expectedIsPowerOfTwo, isPowerOfTwo);
+        }
+
+        private bool IsPowerOfTwo(int number)
+        {
+            while (x%2==0 && x>1)
+            {
+                x /= 2;
+            }
+            return x == 1;
+        }
+    }
+}
