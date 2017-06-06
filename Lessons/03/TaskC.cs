1317d6b6161410cb03fd21db9b760eab007174e5
tbilzou <tbilzou@users.noreply.github.com>
2017-06-06 08:17:45 +0100

    Create TaskC.cs

---
+++ Lessons/03/TaskC.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Lessons._03
+{
+    /// <summary>
+    /// Use ?? operator to implement lazy initialization of _foo backing field 
+    /// in Foo getter.
+    /// </summary>
+    public class TaskC
+    {
+        private static Foo _foo;
+
+        public static void Run()
+        {
+            Console.WriteLine(_foo);
+            Console.WriteLine(Foo);
+        }
+
+        public static Foo Foo
+        {
+            get
+            {
+                return _foo ?? CreateFoo();
+            }
+        }
+        
+        private static Foo CreateFoo()
+        {
+            _foo = new Foo();
+            return _foo;
+        }
+    }
+
+    public class Foo { }
+}
