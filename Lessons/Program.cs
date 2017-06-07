a8d1057940a240aaed59b10de7668ca2d0f6e60b
mchlud <mchlud@RBE03-A082>
2017-06-07 09:08:25 +0100

    Revert "Lesson 03"
    This reverts commit ae5e6e3fe377ce2c139e575e2ffd831a81039f8e.

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -7,10 +7,11 @@ namespace Lessons
         static void Main(string[] args)
         {
             //_01.Runner.Run();
-            //_02.Runner.Run();
-            //_03.Runner.Run();
 
-            Console.WriteLine("Press any key ...");
+
+
+            _02.Runner.Run();
+
             Console.ReadKey(); 
         }
     }
