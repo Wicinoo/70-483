4ec44061e07ecf68f307a0faa4bf8b41ea797676
Nikola Tsartsarov <nikolatsartsarov@gmail.com>
2017-06-07 10:47:33 +0200

    Add files via upload

---
+++ Lessons/03/TaskC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lessons._03
+{
+    /// <summary>
+    /// Use ?? operator to implement lazy initialization of _foo backing field 
+    /// in Foo getter.
+    /// </summary>
+    public class TaskC
+    {
+        private static Foo _foo;
+
+        public static void Run()
+        {
+            try
+            {
+                Console.WriteLine(_foo);
+                Console.WriteLine(Foo);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static Foo Foo
+        {
+            get
+            {
+                return _foo ?? new Foo();
+            }
+        }
+    }
+    public class Foo { }
+}
---
+++ Lessons/03/_01_AccesingSharedData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lessons._03
+{
+    public static class _01_AccesingSharedData
+    {
+        public static void Run()
+        {
+            const int NumberOfIterations = 100000000;
+
+            int value = 0;
+
+            var incrementingTask = Task.Run(() =>
+            {
+                for (int i = 0; i < NumberOfIterations; i++) { value++; }
+            });
+            for (int i = 0; i < NumberOfIterations; i++) { value--; }
+
+            incrementingTask.Wait();
+
+            Console.WriteLine(value);
+        }
+    }
+}
