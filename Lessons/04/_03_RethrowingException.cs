e942654c8d927f02eb93015adbaa3ba83fc36004
Robert Dresler <dresler.robert@gmail.com>
2017-06-08 12:03:13 +0200

    Lesson 04

---
+++ Lessons/04/_03_RethrowingException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lessons._04
+{
+    public static class _03_RethrowingException
+    {
+        public static void Run()
+        {
+            try
+            {
+                int.Parse("NaN");
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine("An FormatException was caught.");
+
+                // Uncomment to check differences
+                //throw;
+
+                throw exception;
+            }
+        }
+    }
+}
