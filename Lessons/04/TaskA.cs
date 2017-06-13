17d78b23b601f07525cdb53dc08cae9e5f9c0e04
psolar <psolar@RBE03-A112>
2017-06-13 21:10:39 +0100

    HW4 - TaskA

--- Lessons/04/TaskA.cs
+++ Lessons/04/TaskA.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.ComponentModel;
 
 namespace Lessons._04
@@ -14,15 +15,58 @@ namespace Lessons._04
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            try
+            {
+                throw new Exception("first exception");
+            }
+            catch (Exception ex1)
+            {
+                try
+                {
+                    ex1.Data.Add("info1", "some info 1");
+                    ex1.Data.Add("info2", "some info 2");
+                    throw new Exception("second exception", ex1);
+                }
+                catch (Exception ex2)
+                {
+                    PrintExceptionDetails(ex2);
+                }
+            }
         }
 
-        private void PrintExceptionDetails(Exception exception)
+        private static void PrintExceptionDetails(Exception exception)
         {
-            foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(exception))
+            foreach (var propertyInfo in exception.GetType().GetProperties())
             {
-                Console.WriteLine("{0}: {1}", property.Name, property.GetValue(exception));
+                if (propertyInfo.Name.Equals("InnerException", StringComparison.InvariantCultureIgnoreCase) && exception.InnerException != null)
+                {
+                    Console.WriteLine("Inner [[");
+                    PrintExceptionDetails(exception.InnerException);
+                    Console.WriteLine("]]");
+                }
+                else if (propertyInfo.Name.Equals("Data", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    PrintExceptionData(exception.Data);
+                }
+                else
+                {
+                    PrintLine(propertyInfo.Name, propertyInfo.GetValue(exception)?.ToString());
+                    //Console.WriteLine("{0}: {1}", propertyInfo.Name, propertyInfo.GetValue(exception));
+                }
             }
+        }
+
+        private static void PrintExceptionData(IDictionary exceptionData)
+        {
+            foreach (var key in exceptionData.Keys)
+            {
+                PrintLine(key.ToString(), exceptionData[key].ToString());
+            }
+        }
+
+        private static void PrintLine(string name, string value)
+        {
+            Console.WriteLine("{0}: {1}", name, value);
         }
     }
 }
