07fe00793ddb44694bf801d7a8f706ac5c430051
Jiří Drdla <drdla.jiri@outlook.com>
2017-06-13 19:58:10 +0200

    Task D.

--- Lessons/04/TaskD.cs
+++ Lessons/04/TaskD.cs
@@ -10,7 +10,43 @@ namespace Lessons._04
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            var buffer = new Buffer();
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    buffer.Push(i);
+                }
+            }
+            catch (BufferFullException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
+    }
+
+    class Buffer
+    {
+        int[] _buffer = new int[2];
+        int _pointer = 0;
+
+        public void Push(int value)
+        {
+            if (_pointer < _buffer.Length)
+            {
+                _buffer[_pointer] = value;
+                _pointer++;
+            }
+            else
+            {
+                throw new BufferFullException($"Unable to push value {value}");
+            }
+        }
+    }
+
+    class BufferFullException : Exception
+    {
+        public BufferFullException(string message) : base(message) { }
     }
 }
