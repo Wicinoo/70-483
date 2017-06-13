601a73ebf98c59b353cbc87d46b1e2abd028a3e4
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2017-06-13 18:13:35 +0200

    Lesson 04 - TaskD

--- Lessons/04/TaskD.cs
+++ Lessons/04/TaskD.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Lessons._04
 {
@@ -10,7 +11,50 @@ namespace Lessons._04
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            var input = Guid.NewGuid().ToString().ToUpper();
+            try
+            {
+                if (input.Contains("A"))
+                    throw new RestrictedSymbolsException(input);
+            }
+            catch (RestrictedSymbolsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    [Serializable]
+    public class RestrictedSymbolsException : Exception, ISerializable
+    {
+        public string Input { get; set; }
+
+        public override string Message
+        {
+            get
+            {
+                return "Input contains restricted characters";
+            }
+        }
+
+        public RestrictedSymbolsException(string input)
+        {
+            Input = input;
+        }
+
+        public RestrictedSymbolsException(string input, string message) : base(message)
+        {
+            Input = input;
+        }
+
+        protected RestrictedSymbolsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            Input = (string)info.GetValue("Input", typeof(string));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Input", Input, typeof(string));
         }
     }
 }
