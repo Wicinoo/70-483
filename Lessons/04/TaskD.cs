2bc39476300954fa5b52b887e54cc17236538cd1
Nikola Tsartsarov <nikolatsartsarov@gmail.com>
2017-06-13 23:54:00 +0200

    Add files via upload

---
+++ Lessons/04/TaskD.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lessons._04
+{
+    /// <summary>
+    /// Create your own custom exception.
+    /// Use it in a reasonable context and simulate throwing and catching.
+    /// </summary>
+
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    [Serializable]
+    class InvalidStudentNameException : Exception
+    {
+        public InvalidStudentNameException()
+        {
+
+        }
+
+        public InvalidStudentNameException(string name)
+            : base(String.Format("Invalid Student Name: {0}", name))
+        {
+
+        }
+
+    }
+
+    public class TaskD
+    {
+        public static void Run()
+        {
+            Student newStudent = null;
+
+            try
+            {
+                newStudent = new Student();
+                newStudent.StudentName = "James007";
+
+                ValidateStudent(newStudent);
+            }
+            catch (InvalidStudentNameException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void ValidateStudent(Student std)
+        {
+            Regex regex = new Regex("^[a-zA-Z]+$");
+
+            if (regex.IsMatch(std.StudentName))
+                throw new InvalidStudentNameException(std.StudentName);
+
+        }
+    }
+}
