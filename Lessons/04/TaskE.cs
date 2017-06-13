35b6675c8d65bd1f4f97a6adab85ff49147a77ee
psolar <psolar@RBE03-A112>
2017-06-13 22:09:27 +0100

    HW4 - TaskE

--- Lessons/04/TaskE.cs
+++ Lessons/04/TaskE.cs
@@ -13,19 +13,39 @@ namespace Lessons._04
         public static void Run1()
         {
             // Implement global exception handling here ...
-
-            throw new InvalidOperationException("Unhandled exception on the main thread.");
+            try
+            {
+                throw new InvalidOperationException("Unhandled exception on the main thread.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-        
+
         public static void Run2()
         {
             // Implement global exception handling for all threads here ...
-
-            Task.Run(() =>
+            try
             {
-                throw new InvalidOperationException("Unhandled exception on a task.");
-            })
-            .Wait();
+                Task.Run(() =>
+                {
+                    throw new InvalidOperationException("Unhandled exception on a task.");
+                })
+                .Wait();
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine("There where {0} exceptions", ex.InnerExceptions.Count);
+
+                if (ex.InnerExceptions.Count > 0)
+                {
+                    foreach (var innerException in ex.InnerExceptions)
+                    {
+                        Console.WriteLine("- {0}", innerException.Message);
+                    }
+                }
+            }
         }
     }
 }
