9384eeb7cb77b5df2590c93080ea4e44940a74aa
psolar <psolar@RBE03-A112>
2017-06-13 19:58:51 +0100

    HW 2 re-commit to correct stream, try 3
    psolar <psolar@RBE03-A112>
    30/05/2017 22:44:20

--- Lessons/02/TaskB.cs
+++ Lessons/02/TaskB.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Lessons._02
 {
@@ -13,7 +15,43 @@ namespace Lessons._02
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            var timer = new Stopwatch();
+            var urls = new[] { "www.visualstudio.com", "www.microsoft.com", "www.google.com" };
+            
+            timer.Start();
+            ProcessSequential(urls);
+            timer.Stop();
+            Console.WriteLine("sequential {0} ms", timer.ElapsedMilliseconds);
+
+            timer.Reset();
+            timer.Start();
+            ProcessParallel(urls);
+            timer.Stop();
+            Console.WriteLine("parallel {0} ms", timer.ElapsedMilliseconds);
+        }
+
+        private static void ProcessSequential(string[] urls)
+        {
+            foreach (var url in urls)
+            {
+                var content = DownloadContentFromWebSite(url);
+            }
+        }
+        
+        private static void ProcessParallel(string[] urls)
+        {
+            Parallel.ForEach(
+                urls,
+                url => { var content = DownloadContentFromWebSite(url); });
+        }
+
+        private static string DownloadContentFromWebSite(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = client.GetStringAsync("http://" + url);
+                return result.Result;
+            }
         }
     }
 }
--- Lessons/02/TaskC.cs
+++ Lessons/02/TaskC.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace Lessons._02
 {
@@ -11,7 +16,55 @@ namespace Lessons._02
     {
         public static void Run()
         {
-            throw new NotImplementedException();
+            //Unsafe();
+
+            Safe();
+        }
+
+        private static void Unsafe()
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                dict.Add(i, i);
+            }
+
+            Parallel.ForEach(dict, pair =>
+            {  
+                var i = pair.Key;
+                var iminus = Math.Max(i - 1, 0);
+                var iplus = Math.Min(i + 1, 100);
+
+                dict[iminus] = dict[i];
+                dict[iplus] = dict[i];
+
+                Console.WriteLine("dict[{0}] = {1} , dict[{2}] = {3}, v[{4}] = {5}", iminus, dict[iminus], i, dict[i], iplus, dict[iplus]);
+            });
+        }
+
+        private static void Safe()
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                dic.Add(i, i);
+            }
+            
+            var dict = new ConcurrentDictionary<int, int>(dic);
+            
+            Parallel.ForEach(dict, pair =>
+            {
+                var i = pair.Key;
+                var iminus = Math.Max(i - 1, 0);
+                var iplus = Math.Min(i + 1, 100);
+
+                dict[iminus] = dict[i];
+                dict[iplus] = dict[i];
+
+                Console.WriteLine("dict[{0}] = {1} , dict[{2}] = {3}, v[{4}] = {5}", iminus, dict[iminus], i, dict[i], iplus, dict[iplus]);
+            });
         }
     }
 }
