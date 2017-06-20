4dda483bc81982094f74a4b72416779ca1f6bce5
Tom067 <Tom067@DESKTOP-OMCN4A0>
2017-06-20 19:24:17 +0200

    Lesson 05 tasks completed

--- Lessons/05/TaskB.cs
+++ Lessons/05/TaskB.cs
@@ -1,4 +1,6 @@
-﻿namespace Lessons._05
+﻿using System;
+
+namespace Lessons._05
 {
     /// <summary>
     /// * Declare an flagged enum type OpeningDays with all days in week. 
@@ -6,15 +8,47 @@
     /// It returns Opened if a value contains today, otherwise Closed.
     /// * Opened and Closed are values of an another custom enum type.
     /// </summary>
+    public static class ExtensionMethods
+    {
+        public static string GetTodayOpening(this OpeningDays openingDays)
+        {
+            var today = DateTime.Today.DayOfWeek.ToString();
+            var openingDayOfWeek = (OpeningDays)Enum.Parse(typeof(OpeningDays), today, true);
+
+            return openingDays.HasFlag(openingDayOfWeek) 
+                ? ShopState.Opened.ToString()
+                : ShopState.Closed.ToString();
+        }
+    }
+
+    [Flags]
+    public enum OpeningDays
+    {
+        None = 16,
+        Monday = 0,
+        Tuesday = 1,
+        Wednesday = 2,
+        Thursday = 4,
+        Friday = 6,
+        Saturday = 8,
+        Sunday = 12
+    }
+
+    public enum ShopState
+    {
+        Opened,
+        Closed
+    }
+
     public class TaskB
     {
         public static void Run()
         {
-            //Console.WriteLine($"Never open: {OpeningDays.None.GetTodayOpening()}");
-            //Console.WriteLine($"Only Mondays: {OpeningDays.Monday.GetTodayOpening()}");
-            //Console.WriteLine($"From Monday to Wednesday: {(OpeningDays.Monday | OpeningDays.Tuesday | OpeningDays.Wednesday).GetTodayOpening()}");
-            //Console.WriteLine($"All weekdays: {(OpeningDays.Monday | OpeningDays.Tuesday | OpeningDays.Wednesday | OpeningDays.Thursday | OpeningDays.Friday).GetTodayOpening()}");
-            //Console.WriteLine($"Only weekends: {(OpeningDays.Saturday | OpeningDays.Sunday).GetTodayOpening()}");
+            Console.WriteLine($"Never open: {OpeningDays.None.GetTodayOpening()}");
+            Console.WriteLine($"Only Mondays: {OpeningDays.Monday.GetTodayOpening()}");
+            Console.WriteLine($"From Monday to Wednesday: {(OpeningDays.Monday | OpeningDays.Tuesday | OpeningDays.Wednesday).GetTodayOpening()}");
+            Console.WriteLine($"All weekdays: {(OpeningDays.Monday | OpeningDays.Tuesday | OpeningDays.Wednesday | OpeningDays.Thursday | OpeningDays.Friday).GetTodayOpening()}");
+            Console.WriteLine($"Only weekends: {(OpeningDays.Saturday | OpeningDays.Sunday).GetTodayOpening()}");
         }
     }
 }
--- Lessons/05/TaskC.cs
+++ Lessons/05/TaskC.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lessons._05
 {
@@ -13,11 +14,11 @@ namespace Lessons._05
         {
             var week = new Week();
 
-            var allDaysOfWeek = new DayOfWeek[0]; // #3 get all DayOfWeek values 
+            var allDaysOfWeek = Enum.GetValues(typeof(DayOfWeek)); // #3 get all DayOfWeek values 
 
-            foreach (var dayOfWeek in allDaysOfWeek)
+            foreach (DayOfWeek dayOfWeek in allDaysOfWeek)
             {
-                // #4 Console.WriteLine(week[dayOfWeek]);
+                Console.WriteLine(week[dayOfWeek]);
             }
         }
     }
@@ -28,9 +29,22 @@ namespace Lessons._05
         {
             new DayMessageRule(day => day < DateTime.Now.DayOfWeek, day => $"{day} is gone."),
             new DayMessageRule(day => day == DateTime.Now.DayOfWeek, day => $"{day} is today."),
-
+            new DayMessageRule(day => day > DateTime.Now.DayOfWeek, day => $"{day} is coming.")
             // #1 add a rule for "Day is coming."
         };
+
+        public string this[DayOfWeek index]
+        {
+            get
+            {
+                return GetMessage(index);
+            }
+        }
+
+        private string GetMessage(DayOfWeek index)
+        {
+            return dayMessageRules.Where(x => x.Predicate(index)).First().Message(index);
+        }
 
         // #2 add an indexer that returns a message for a DayOfWeek value
 
