51f94b8e12813ab17edff5ef35352b1fe1b998fd
psolar <psolar@RBE03-A112>
2017-06-20 22:07:48 +0100

    HW5 TaskA

--- Lessons/05/TaskA.cs
+++ Lessons/05/TaskA.cs
@@ -1,4 +1,6 @@
-﻿namespace Lessons._05
+﻿using System;
+
+namespace Lessons._05
 {
     /// <summary>
     /// Print the ancestors of FooClass and FooStruct.
@@ -9,10 +11,32 @@
         public static void Run()
         {
             // Print "The ancestor type of FooClass is ?."
+            var fooClass = new FooClass();
+            var fooClassAncFullName = string.Empty;
+
+            var fooClassBaseType = fooClass.GetType().BaseType;
+            if (fooClassBaseType != null)
+            {
+                fooClassAncFullName = fooClassBaseType.FullName;
+            }
+
+            Console.WriteLine($"The ancestor type of {fooClass.GetType().FullName} is {fooClassAncFullName}");
+
             // Print "The ancestor type of FooStruct is ?."
+            var fooStruct = new FooStruct();
+            var fooStructAncFullName = string.Empty;
+
+            var fooStructBaseType = fooStruct.GetType().BaseType;
+            if (fooStructBaseType != null)
+            {
+                fooStructAncFullName = fooStructBaseType.FullName;
+            }
+
+            Console.WriteLine($"The ancestor type of {fooStruct.GetType().Name} is {fooStructAncFullName}");        
         }
     }
 
     class FooClass { }
+
     struct FooStruct { }
 }
