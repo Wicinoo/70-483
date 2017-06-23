964476e54d67dc2c823faa5f56d454f65995fb00
Robert Dresler <dresler.robert@gmail.com>
2017-06-23 09:46:35 +0200

    Lesson 06

---
+++ Lessons/06/TaskC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lessons._06
+{
+    /// <summary>
+    /// Implement MyDynamicSession class by using DynamicObject as a base class. 
+    /// Check the output in Run() method.
+    /// </summary>
+    public class TaskC
+    {
+        public static void Run()
+        {
+
+            //dynamic myDynamicSession = new MyDynamicSession();
+
+            //myDynamicSession.Key1 = "valueForKey1";
+            //Console.WriteLine(myDynamicSession.Key1);   // valueForKey1
+
+            //myDynamicSession.Key2 = DateTime.Now;
+            //Console.WriteLine(myDynamicSession.Key2);   // <Today date and time>
+
+            //Console.WriteLine(myDynamicSession.NonexistingKey ?? "null");   // null
+        }
+    }
+}
