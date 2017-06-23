187b68a3e25788f54e032cb20fcfdbaf1031f9bb
Robert Dresler <dresler.robert@gmail.com>
2017-06-23 09:46:35 +0200

    Lesson 06

---
+++ Lessons/06/_01_UserDefinedConversions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lessons._06
+{
+    public static class _01_UserDefinedConversions
+    {
+        public static void Run()
+        {
+            Money money = new Money(42.42M);
+            decimal amount = money;
+            int truncatedAmount = (int)money;
+
+            Console.WriteLine($"Amount: {amount}");
+            Console.WriteLine($"Truncated amount: {truncatedAmount}");
+        }
+
+        class Money
+        {
+            public Money(decimal amount)
+            {
+                Amount = amount;
+            }
+            public decimal Amount { get; set; }
+
+            public static implicit operator decimal(Money money)
+            {
+                return money.Amount;
+            }
+            public static explicit operator int(Money money)
+            {
+                return (int)money.Amount;
+            }
+        }
+
+    }
+}
