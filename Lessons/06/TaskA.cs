3bc54671cd79323c41a8e2feecfde9dada7ee9da
pvitvera <pvitvera@RBE02-A072>
2017-06-27 20:28:22 +0100

    hw

---
+++ Lessons/06/TaskA.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Newtonsoft.Json;
+
+namespace Lessons._06
+{
+
+    /// <summary>
+    /// Implement MorseCodeConverter to pass all tests.
+    /// Implement MorseCode class to pass all tests. Use MorseCodeConverter.
+    /// </summary>
+    public class TaskA
+    {
+        public class MorseCodeConverterTests
+        {
+            // Task: Implement MorseCodeConverter.ToMorseCode
+            // Don't care about invalid characters.
+
+            [Theory]
+            [InlineData("", "")]
+            [InlineData("a", ".-")]
+            [InlineData("A", ".-")]
+            [InlineData("42", "....-|..---")]
+            [InlineData("Keep@it@secret!", "-.-|.|.|.--.|.--.-.|..|-|.--.-.|...|.|-.-.|.-.|.|-|-.-.--")]
+            public void ToMorseCode_ForGivenText_ShouldReturnExpected(string text, string expectedMorseCode)
+            {
+                var morseCode = MorseCodeConverter.ToMorseCode(text);
+                Assert.Equal(expectedMorseCode, morseCode);
+            }
+
+            // Task: Implement MorseCodeConverter.FromMorseCode
+            // Don't care about invalid characters.
+
+            [Theory]
+            [InlineData("", "")]
+            [InlineData("....|.|.-..|.--.|--|.", "helpme")]
+            [InlineData("...|.|-.-.|.-.|.|-|...--|..---|----.|----.|---..|--...", "secret329987")]
+            public void FromMorseCode_ForGivenText_ShouldReturnExpected(string morseCode, string expectedText)
+            {
+                var text = MorseCodeConverter.FromMorseCode(morseCode);
+                Assert.Equal(expectedText, text);
+            }
+        }
+
+        public class MorseCodeTests
+        {
+            [Theory]
+            [InlineData("", "")]
+            [InlineData("a", ".-")]
+            [InlineData("A", ".-")]
+            [InlineData("42", "....-|..---")]
+            [InlineData("Keep@it@secret!", "-.-|.|.|.--.|.--.-.|..|-|.--.-.|...|.|-.-.|.-.|.|-|-.-.--")]
+            public void Ctor_ForGivenTextAndDefaultDelimiter_ShouldReturnExpectedFromImplicitConversion(string text, string expectedMorseCodeText)
+            {
+                //throw new NotImplementedException();
+
+                var morseCode = new MorseCode(text);
+                string morseCodeText = morseCode.resText;
+
+                Assert.Equal(expectedMorseCodeText, morseCodeText);
+            }
+
+            [Fact]
+            public void Ctor_ForGivenTextAndExplicitDelimiter_ShouldReturnExpectedFromImplicitConversion()
+            {
+                throw new NotImplementedException();
+
+                var morseCode = new MorseCode("helpme", "*");
+                string morseCodeText = morseCode.resText;
+                var expectedMorseCodeText = "....*.*.-..*.--.*--*.";
+
+                Assert.Equal(expectedMorseCodeText, morseCodeText);
+            }
+
+            [Fact]
+            public void ImplicitConversionFromString_ForGivenString_ShouldReturnExpectedFromImplicitConversion()
+            {
+                throw new NotImplementedException();
+
+                MorseCode morseCode = "helpme";
+                string morseCodeText = morseCode.resText;
+                var expectedMorseCodeText = "....|.|.-..|.--.|--|.";
+
+                Assert.Equal(expectedMorseCodeText, morseCodeText);
+            }
+        }
+
+        public class MorseCode
+        {
+            public string resText;
+            public MorseCode(string morseCodeWithDelimiters, string _morseDelimiter = MorseCodeConstants.MorseCodesSeparator)
+            {
+                //throw new NotImplementedException();
+
+                resText = MorseCodeConverter.FromMorseCode(morseCodeWithDelimiters);
+            }
+
+            public static implicit operator string(MorseCode morseCode)
+            {
+
+                return morseCode.resText;
+            }
+
+            public static implicit operator MorseCode(string text)
+            {
+                return new MorseCode(text);
+            }
+
+            // Implement implicit operator for converting from string to MorseCode.
+            // Implement implicit operator for converting from MorseCode to string.
+        }
+
+        public static class MorseCodeConverter
+        {
+            public static string ToMorseCode(string text)
+            {
+                Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(MorseCodeConstants.MorseAlphabetAsJson);
+
+                string moreCodeRes = "";
+
+                for (int i = 0; i < text.Length - 1; i++)
+                {
+                    moreCodeRes += values[text[i].ToString()] + MorseCodeConstants.MorseCodesSeparator;
+                }
+
+                moreCodeRes += values[text[text.Length - 1].ToString()];
+
+                return moreCodeRes;
+            }
+
+            public static string FromMorseCode(string morseCode)
+            {
+                Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(MorseCodeConstants.MorseAlphabetAsJson);
+
+                string[] letters = morseCode.Split(MorseCodeConstants.MorseCodesSeparator[0]);
+                string wordRes = "";
+
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    wordRes += values.FirstOrDefault(x => x.Value == letters[i]).Key;
+                }
+
+                return wordRes;
+            }
+        }
+
+        public class MorseCodeConstants
+        {
+            public const string MorseCodesSeparator = "|";
+
+            public const string MorseAlphabetAsJson =
+                @"
+{
+  ""0"": ""-----"",
+  ""1"": "".----"",
+  ""2"": ""..---"",
+  ""3"": ""...--"",
+  ""4"": ""....-"",
+  ""5"": ""....."",
+  ""6"": ""-...."",
+  ""7"": ""--..."",
+  ""8"": ""---.."",
+  ""9"": ""----."",
+  ""a"": "".-"",
+  ""b"": ""-..."",
+  ""c"": ""-.-."",
+  ""d"": ""-.."",
+  ""e"": ""."",
+  ""f"": ""..-."",
+  ""g"": ""--."",
+  ""h"": ""...."",
+  ""i"": "".."",
+  ""j"": "".---"",
+  ""k"": ""-.-"",
+  ""l"": "".-.."",
+  ""m"": ""--"",
+  ""n"": ""-."",
+  ""o"": ""---"",
+  ""p"": "".--."",
+  ""q"": ""--.-"",
+  ""r"": "".-."",
+  ""s"": ""..."",
+  ""t"": ""-"",
+  ""u"": ""..-"",
+  ""v"": ""...-"",
+  ""w"": "".--"",
+  ""x"": ""-..-"",
+  ""y"": ""-.--"",
+  ""z"": ""--.."",
+  ""."": "".-.-.-"",
+  "","": ""--..--"",
+  ""?"": ""..--.."",
+  ""!"": ""-.-.--"",
+  ""-"": ""-....-"",
+  ""/"": ""-..-."",
+  ""@"": "".--.-."",
+  ""("": ""-.--."",
+  "")"": ""-.--.-""
+}";
+        }
+    }
+}
---
+++ Lessons/06/TaskC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Lessons._06
+{
+    /// <summary>
+    /// Implement MyDynamicSession class by using DynamicObject as a base class. 
+    /// Check the output in Run() method.
+    /// </summary>
+    public class TaskC
+    {
+        public static void Run()
+        {
+
+            dynamic myDynamicSession = new MyDynamicSession();
+
+            myDynamicSession.Key1 = "valueForKey1";
+            Console.WriteLine(myDynamicSession.Key1);   // valueForKey1
+
+            myDynamicSession.Key2 = DateTime.Now;
+            Console.WriteLine(myDynamicSession.Key2);   // <Today date and time>
+
+            Console.WriteLine(myDynamicSession.NonexistingKey ?? "null");   // null
+        }
+    }
+
+    public class MyDynamicSession : DynamicObject
+    {
+        private Dictionary<string, object> MyDictionary = new Dictionary<string, object>();
+
+        public override bool TryGetMember(GetMemberBinder binder, out object res){
+            bool keyExists = MyDictionary.ContainsKey(binder.Name);
+            res = keyExists ? MyDictionary[binder.Name] : null;
+            return keyExists;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object res){
+            MyDictionary[binder.Name] = res;
+            return true;
+        }
+    }
+}
