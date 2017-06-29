54ed928e6eb715cd9b7467452d6418052627d009
Robert Dresler <dresler.robert@gmail.com>
2017-06-29 14:37:28 +0200

    Lesson 07

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -6,14 +6,15 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-			//_01.Runner.Run();
-			//_02.Runner.Run();
-			//_03.Runner.Run();
-			//_04.Runner.Run();
-			//_05.Runner.Run();
-			_06.Runner.Run();
+            //_01.Runner.Run();
+            //_02.Runner.Run();
+            //_03.Runner.Run();
+            //_04.Runner.Run();
+            //_05.Runner.Run();
+            //_06.Runner.Run();
+            //_07.Runner.Run();
 
-			Console.WriteLine("Press any key ...");
+            Console.WriteLine("Press any key ...");
             Console.ReadKey(); 
         }
     }
