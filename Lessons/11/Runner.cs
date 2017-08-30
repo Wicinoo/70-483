6164f3463b94bed899a85147d54727a37f5a5ed6
Nikola Zeman <zemann@seznam.cz>
2017-08-30 06:00:31 +0200

    11 TaskA

--- Lessons/11/Runner.cs
+++ Lessons/11/Runner.cs
@@ -1,10 +1,11 @@
-﻿namespace Lessons._11
+﻿using Lessons._11.Tasks;
+namespace Lessons._11
 {
     public class Runner
     {
         public static void Run()
         {
-            //TaskA.Run();
+            TaskA.Run();
             //TaskB.Run();
             //TaskC.Run();
         }
