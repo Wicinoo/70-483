5eabc218dd7cb27d79bde12832132b49de666b25
tbilzou <tbilzou@users.noreply.github.com>
2017-08-30 06:49:22 +0100

    Create TaskB.cs

---
+++ Lessons/09/TaskB.cs
@@ -0,0 +1,44 @@
+#if DEBUG
+	#define MySymbol
+#endif
+
+using System;
+
+
+namespace Lessons._09
+{
+	/// <summary>
+	/// Use compiler directives to:
+	/// - define a symbol MySymbol just for Debug configuration.
+	/// - if MySymbol is defined, print "MySymbol is defined" within Run() method.
+	/// - put a warning "This code is obsolete" somewhere.
+	/// - trigger an error in Debug configuration at some point of code.
+	/// - disable warnings for a part of code for non-Debug configuration.
+	/// - try to run the task in Debug and Release configuration.
+	/// </summary>
+
+
+
+	class TaskB
+    {
+#warning This code is obsolete
+		public static void Run()
+        {
+#if DEBUG
+	#error Error
+#endif
+
+#if MySymbol
+			Console.WriteLine("MySymbol is defined");
+#endif
+
+#if DEBUG
+	#pragma warning disable
+#endif
+#if DEBUG
+	#pragma warning restore
+#endif
+
+		}
+	}
+}
