48e0c522b25ef5c8dc24f82c765c866be96a377f
tbilzou <tbilzou@users.noreply.github.com>
2017-08-30 06:45:51 +0100

    Create TaskB.cs

---
+++ Lessons/08/TaskB.cs
@@ -0,0 +1,86 @@
+using System;
+using Xunit;
+using System.Reflection;
+using System.Linq;
+
+namespace Lessons._08
+{
+    /// <summary>
+    /// Implement GetEnumValueAttribute() extension method to get an attribute defined for an enum value.
+    /// Make all tests passed.
+    /// Make multiple usage of FooAttribute unallowed.
+    /// </summary>
+    public class TaskB
+    {
+        [Fact]
+        public void GetEnumValueAttribute_ForNonEnumValue_ShouldThrowArgumentException()
+        {
+			Assert.Throws<ArgumentException>(() => new object().GetEnumValueAttribute<FooAttribute>());
+		}
+
+        [Fact]
+        public void GetEnumValueAttribute_ForValueWithoutAttributes_ShouldReturnNull()
+        {
+            var enumValue = TestEnum.ValueWithoutAttributes;
+            var result = enumValue.GetEnumValueAttribute<FooAttribute>();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetEnumValueAttribute_ForValueWithDifferentAttribute_ShouldReturnNull()
+        {
+            var enumValue = TestEnum.ValueWithBar;
+            var result = enumValue.GetEnumValueAttribute<FooAttribute>();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetEnumValueAttribute_ForValueWithFoo_ShouldReturnFooAttributeInstance()
+        {
+            var enumValue = TestEnum.ValueWithFoo;
+            var result = enumValue.GetEnumValueAttribute<FooAttribute>();
+
+            Assert.IsType<FooAttribute>(result);
+        }
+
+        enum TestEnum
+        {
+            ValueWithoutAttributes,
+            [Foo]
+            ValueWithFoo,
+            [Bar]
+            ValueWithBar
+        }
+
+        class FooAttribute : Attribute
+        {
+        }
+
+        class BarAttribute : Attribute
+        {
+        }
+    }
+
+    static class EnumExtensions
+    {
+        public static TAttribute GetEnumValueAttribute<TAttribute>(this object enumValue) where TAttribute: Attribute
+        {
+			if (!enumValue.GetType().IsEnum)
+				throw new ArgumentException();
+
+			MemberInfo memberInfo = enumValue.GetType()
+				.GetMember(enumValue.ToString())
+				.FirstOrDefault();
+
+			if (memberInfo != null)
+			{
+				return memberInfo.GetCustomAttributes<TAttribute>().FirstOrDefault();
+			}
+
+			return null;
+		}
+    }
+
+}
