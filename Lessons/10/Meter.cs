1f70169a2730217892f4c44a4f20d02182a1e42b
Jiří Drdla <drdla.jiri@casid.cz>
2017-09-05 22:56:00 +0200

    Task A.

---
+++ Lessons/10/Meter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lessons._10
+{
+    public static class Meter
+    {
+        public static TimeSpan Measure(Action actionToMeasure)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            actionToMeasure.Invoke();
+            watch.Stop();
+            return watch.Elapsed;
+        }
+    }
+}
---
+++ Lessons/10/Runner.cs
@@ -0,0 +1,10 @@
+﻿namespace Lessons._10
+{
+    public class Runner
+    {
+        public static void Run()
+        {
+            //TaskA.Run();
+        }
+    }
+}
