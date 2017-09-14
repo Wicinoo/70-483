3d6c899f78be9aff777c5615a5021935d1d466f7
jsmolik <toss4win@gmail.com>
2017-09-14 18:36:20 +0200

    Added lesson 14
    lesson 14

---
+++ Lessons/14/TaskD.cs
@@ -0,0 +1,20 @@
+﻿//Task D: Read XML from http://www.currency-iso.org/dam/downloads/lists/list_one.xml and write all entities in format:
+//ALGERIA, Algerian Dinar, DZD
+
+using System;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace Lessons._14
+{
+    static class TaskD
+    {
+        public static void Run()
+        {
+            //read stuff from URL above
+
+            //read and print the stuff in format above
+        }
+    }
+}
